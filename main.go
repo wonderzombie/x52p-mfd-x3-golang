4ab91d376cd2ae460235352bf1aec7d344e16554
wonderzombie <wonderzombie@gmail.com>
2014-08-12 13:28:20 -0700

    Successfully calling InitializeDevice?
    I think this is working now, the initialize call. It's not crashing
    anyway.
    
    Next I see if I can enumerate devices.

--- main.go
+++ main.go
@@ -4,31 +4,56 @@ import (
 	"fmt"
 	"syscall"
 	"os"
+	"unsafe"
+	// "strings"
 )
 
+var deviceTypeX52Pro = "29DAD506-F93B-4F20-85FA-1E02C04FAC17";
+var appName = "x52p-mfd-x3-golang"
+var dllPath = "DirectOutput.dll"
+
+func TestBeep() {
+	beepFunc := syscall.MustLoadDLL("user32.dll").MustFindProc("MessageBeep")
+	beepFunc.Call(0xffffffff)
+}
+
+func SoftButtonChangeCallback(a... uintptr) {
+	fmt.Printf("all the things: %q", a)
+}
+
 func main() {
-	// dll, err := syscall.LoadDLL("kernel32.dll")
-	dll := syscall.MustLoadDLL("DirectOutput.dll")
-	// if err != nil {
-	// 	fmt.Println("Error while loading DirectOutput.dll:", err)
-	// 	os.Exit(1)
-	// }
-	fmt.Println(dll)
+	TestBeep()
 
-	proc, err := dll.FindProc("DirectOutput_Initialize")
-
+	lazyDll := syscall.NewLazyDLL(dllPath)
+	err := lazyDll.Load()
 	if err != nil {
-		fmt.Println("Error while trying to find DirectOutput_Initialize:", err);
+		fmt.Println(err)
 		os.Exit(1)
 	}
+	fmt.Println(lazyDll)
+
+	proc := lazyDll.NewProc("DirectOutput_Initialize")
 
 	fmt.Println(proc)
 
-	err = dll.Release()
-	if err != nil {
-		fmt.Println("error while releasing DLL:", err)
-		os.Exit(1)
-	}
+	runes := []rune(appName)
+	p := uintptr(unsafe.Pointer(&runes))
+	r1, r2, errNo := proc.Call(p)
+
+	// Takes a long time!
+
+	fmt.Printf("%q\n", r1)
+	fmt.Printf("%q\n", r2)
+	fmt.Printf("%q\n", errNo)
+
+	// cbackPtr := syscall.NewCallback(SoftButtonChangeCallback)
+
+	// setCallbackProc := lazyDll.NewProc("DirectOutput_RegisterDeviceChangeCallback")
+	// fmt.Println(setCallbackProc)
+
+	// setCallbackProc.Call()
+
+
 
 	os.Exit(0)
 }
